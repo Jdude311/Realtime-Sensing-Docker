4937ea8ca7a75d582c09a073179b69e04495c896
Jdude311 <jadenjohn.ch@gmail.com>
2022-10-13 18:54:27 -0400

    Moved config files to host

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -29,5 +29,5 @@ db = new Mongo().getDB("sensors");
 db.createCollection("realtime", { capped: false });
 
 db.realtime.insert([
-  {"time": new Date(), "location": "RDHS 312 (SCD30 TEST 1)","tvoc": 0, "co2": 807.117, "temperature": 27.0268, "humidity": 28.1662}
+  {"time": Date.now(), "location": "RDHS 312 (SCD30 TEST 1)","tvoc": 0, "co2": 807.117, "temperature": 27.0268, "humidity": 28.1662}
 ])
